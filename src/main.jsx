87ece276c232300b5781fc7dc494a0e5bef99d37
Seb <sebastian.ledebur@gmail.com>
2026-05-20 05:00:56 +0200

    Route through App.jsx for viewport scaling and CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import WalkCycle from './WalkCycle.jsx'
+import App from './App'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <WalkCycle />
+    <App />
   </StrictMode>,
 )
